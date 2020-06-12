0bc63d68cc66435fb247dd4550b7b19bdec84959
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2020-06-12 13:18:20 -0400

    clickable controls, touch controls dont seem to work

--- canvas_game_oo.js
+++ canvas_game_oo.js
@@ -19,15 +19,25 @@ function Game(elementId, width, height) {
   this.frameN = 0;
   this.input = new Object()
   this.input.keys = [];
+  this.buttons = {};
+  this.input.x = false;
+  this.input.y = false;
 
   var element = document.getElementById(elementId);
   // TODO inserts before first node, make more flexible?
   element.insertBefore(this.canvas, element.childNodes[0]);
 
   this.start = function () {
-    this.player = new Component(this.context, this.settings.startX, this.settings.startY,
-                                this.settings.playerWidth, this.settings.playerHeight,
-                                this.settings.playerColor, true);
+    //CONTROLS
+    var w = 20, h = 20
+    var x = this.canvas.width / 2 - w * 3 / 2,
+        y = this.canvas.height - h * 4;
+    this.buttons = {
+      up: new Component(this.context,   x + w,     y,         w, h, "blue"),
+      down: new Component(this.context, x + w,     y + h * 2, w, h, "blue"),
+      left: new Component(this.context, x,         y + h,     w, h, "blue"),
+      right: new Component(this.context,x + w * 2, y + h,     w, h, "blue")
+    };
 
     // Disable the arrow keys from scrolling
     window.addEventListener('keydown', function (e) {
@@ -35,12 +45,41 @@ function Game(elementId, width, height) {
         e.preventDefault();
       }
     });
+
+    // KEYBOARD
     window.addEventListener('keydown', function (e) {
       self.input.keys[e.keyCode] = true;
     })
     window.addEventListener('keyup', function (e) {
       self.input.keys[e.keyCode] = false;
     })
+
+    //MOUSE AND TOUCH
+    this.canvas.addEventListener('mousedown', function (e) {
+      self.input.x = e.offsetX;
+      self.input.y = e.offsetY;
+    })
+    this.canvas.addEventListener('mouseup', function (e) {
+      self.input.x = false;
+      self.input.y = false;
+    })
+    /* TODO touch events broken
+    window.addEventListener('touchstart', function (e) {
+      e.preventDefault();
+      self.player.setSpeedX(-1);
+      self.input.x = e.offsetX;
+      self.input.y = e.offsetY;
+    })
+    this.canvas.addEventListener('touchend', function (e) {
+      self.input.x = false;
+      self.input.y = false;
+    })
+    */
+
+    // PLAYER AND GAME LOOP
+    this.player = new Component(this.context, this.settings.startX, this.settings.startY,
+                                this.settings.playerWidth, this.settings.playerHeight,
+                                this.settings.playerColor, true);
     this.interval = window.setInterval(this.update, 1000 / this.settings.fps);
   }
 
@@ -50,18 +89,38 @@ function Game(elementId, width, height) {
 
   this.update = function () {
     // To be called by window.setInterval, so we must use self to not lose binding
-    self.context.clearRect(0, 0, self.canvas.width, self.canvas.height);
-    self.context.fillStyle = self.settings.backgroundColor;
-    self.context.fillRect(0, 0, self.canvas.width, self.canvas.height);
-    
-    self.player.setSpeedX(0);
-    self.player.setSpeedY(0);
+
+    // INPUT CHECK
+    //self.player.setSpeedX(0);
+    //self.player.setSpeedY(0);
+
+    if (self.input.x) {
+      if (self.buttons.left.clicked(self.input.x, self.input.y)) {
+        self.player.setSpeedX(-1);
+      } else if (self.buttons.right.clicked(self.input.x, self.input.y)) {
+        self.player.setSpeedX(1);
+      } else if (self.buttons.up.clicked(self.input.x, self.input.y)) {
+        self.player.setSpeedY(-1);
+      } else if (self.buttons.down.clicked(self.input.x, self.input.y)) {
+        self.player.setSpeedY(1);
+      }
+    } else {
+      self.player.setSpeedX(0);
+      self.player.setSpeedY(0);
+    }
 
     if (self.input.keys[37]) { self.player.setSpeedX(-1); }; //left
     if (self.input.keys[38]) { self.player.setSpeedY(-1); }; //up
     if (self.input.keys[39]) { self.player.setSpeedX(1); }; //right
     if (self.input.keys[40]) { self.player.setSpeedY(1); }; //down
+
+    // UPDATE AND REDRAW
+    self.context.clearRect(0, 0, self.canvas.width, self.canvas.height);
+    self.context.fillStyle = self.settings.backgroundColor;
+    self.context.fillRect(0, 0, self.canvas.width, self.canvas.height);
+
     self.player.update();
+    for (button in self.buttons) { self.buttons[button].update(); };
   }
 
   this.onInputEvent = function (e) {
@@ -101,6 +160,18 @@ function Component(context, x, y, width, height, color, isMobile) {
   this.draw = function () {
     this.context.fillStyle = this.color;
     this.context.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  this.clicked = function (x, y) {
+    var l = this.x;
+    var r = this.x + this.width;
+    var t = this.y;
+    var b = this.y + this.height;
+    if ((r < x) || (l > x) || (b < y) || (t > y)) {
+      return false;
+    } else {
+      return true;
+    }
   }
 }
 

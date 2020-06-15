12f5dbfde7e1f1e17e195c0d4ebe5c677cf64535
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2020-06-15 18:52:17 -0400

    removed buttons

--- canvas_game.php
+++ canvas_game.php
@@ -2,22 +2,7 @@
   $title = "Canvas Game";
 ?>
 
-
-<style>
-button {
-  color:red;
-  height:4em;
-  text-align:center;
-  padding: auto;
-  margin:auto;
-}
-</style>
 <div id="game">
-<br>
-<button onmousedown="moveleft()" onmouseup="stopMove()" ontouchstart="moveleft()">LEFT</button>
-<button onmousedown="moveright()" onmouseup="stopMove()" ontouchstart="moveright()">RIGHT</button><br>
-<button onmousedown="moveup()" onmouseup="stopMove()" ontouchstart="moveup()">UP</button>
-<button onmousedown="movedown()" onmouseup="stopMove()" ontouchstart="movedown()">DOWN</button>
 </div>
 
 <script src="canvas_game_oo.js">
